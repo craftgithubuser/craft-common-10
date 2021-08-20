d4a7444f1964068764919f261648ee64a14647c7
tsion <tsiyonk21@gmail.com>
2021-08-20 01:57:16 -0400

    Java Project From Eclipse

---
+++ Project/src/com/tsiyon/GitDemo.java
@@ -0,0 +1,11 @@
+package com.tsiyon;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+System.out.println("Welcome to Java World");
+	}
+
+}
