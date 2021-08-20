0b402d09b0d76e3bffd9500b723bd0f2b7b7ed42
TinCraft/study-group <ework4358@gmail.com>
2021-08-20 01:15:49 -0400

    pushE working

---
+++ pushE/src/pushE/cal.java
@@ -0,0 +1,25 @@
+package pushE;
+
+public class cal {
+
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			int val1 =20;
+			int val2 =10;
+			
+			int add = val1 + val2;
+			int sub = val1-val2;
+			int mult =  val1 * val2;
+			int div  = val1/val2;
+
+			System.out.println("Addition of two number: " + add);
+			System.out.println("Subtration of two number: " + sub);
+			System.out.println("Multiplication of two number: " + mult);
+			System.out.println("Division of two number: " + div);
+			
+			
+		}
+	
+
+}
