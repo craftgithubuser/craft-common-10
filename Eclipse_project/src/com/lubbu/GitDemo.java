a5826990b75113caa5c7a4eff15c9d4380b84e9f
Lubbuugob <Lubbuugob@gmail.com>
2021-08-19 22:53:41 -0400

    update made

--- Eclipse_project/src/com/lubbu/GitDemo.java
+++ Eclipse_project/src/com/lubbu/GitDemo.java
@@ -5,7 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello world!");
+		System.out.println("Hello world! ");
 
 	}
 
