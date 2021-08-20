4384eabaf2e57c70b5d78f88a3c17b91a49c7801
LeulBulcha <leul.bulcha23@gmail.com>
2021-08-19 22:27:35 -0400

    test2

--- Craft_First/src/com/craft/git/GitDemo.java
+++ Craft_First/src/com/craft/git/GitDemo.java
@@ -5,7 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 
 		//System.out.println("Welcome to java world");
-
+		//test run 
 		for (int i = 0; i < 100; i++) {
 
 			if (i % 2 == 0) {
