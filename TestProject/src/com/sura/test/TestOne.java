5b9fd5251ffebe04753c802524a4d75500be68f2
sura21 <surafeldemse@gmail.com>
2021-08-20 23:29:16 -0400

    hello

---
+++ TestProject/src/com/sura/test/TestOne.java
@@ -0,0 +1,10 @@
+package com.sura.test;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TestProject/src/com/sura/test/TestTwo.java
@@ -0,0 +1,10 @@
+package com.sura.test;
+
+public class TestTwo {
+
+	public static void main(String[] args) {
+		System.out.println("Test Two");
+
+	}
+
+}
