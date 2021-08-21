a1ed23812b1dcf098f123c52bf18406e4f58d5df
yohanestadde@gmail.com <yohanestadde@gmail.com>
2021-08-21 00:03:04 -0500

    This is commited from eclips

---
+++ Yohanes-Craft-10/src/YohaPkg/YohaClass.java
@@ -0,0 +1,11 @@
+package YohaPkg;
+
+public class YohaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Java World");
+
+	}
+
+}
