67b5a832aa856f4aa7d6f9b7df4693ae54dd2111
sura21 <surafeldemse@gmail.com>
2021-08-21 00:46:17 -0400

    Testsure

---
+++ SureTest/src/ssm/sur/javaproject.java
@@ -0,0 +1,12 @@
+package ssm.sur;
+
+public class javaproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("welcome to java world");
+System.out.print("Sure");
+
+	}
+
+}
