86cc137731d779a8d66272fe0fc0951981d8fee0
tms21-13 <tamrat202097@gmail.com>
2021-08-22 01:06:19 -0400

    Java first commit

---
+++ HeloWorld/src/craft10/PracticalProject.java
@@ -0,0 +1,14 @@
+package craft10;
+
+public class PracticalProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		System.out.println("Craft.commn7")
+		
+		
+
+	}
+
+}
