bc7dda553fc1c6cc75551fed0b5e6adafee79bd8
hime-ad <hadmassu6@gmail.com>
2021-08-23 14:15:15 -0400

    Java project from Eclipse

---
+++ HelloWorldproj1/src/hime/pack/GitFile.java
@@ -0,0 +1,12 @@
+package hime.pack;
+
+public class GitFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		System.out.println("welcome to java world");
+		System.out.println("craft-common-10");
+	}
+
+}
