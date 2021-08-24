a3a1e89fad734430a142e36597f01bdc9b2a45c2
alexotad <alexo.tadess@gmail.com>
2021-08-23 22:13:31 -0400

    commit

---
+++ Eclipse/src/com/icraft/Javaproject.java
@@ -0,0 +1,11 @@
+package com.icraft;
+
+public class Javaproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to java");
+        System.out.println("java project");
+	}
+
+}
