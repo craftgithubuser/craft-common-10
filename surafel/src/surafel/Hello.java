3335d56f2e79f2da44aaf1410f3548767d76f292
sura21 <surafeldemse@gmail.com>
2021-08-25 00:46:26 -0400

    another surafel commot

--- surafel/src/surafel/Hello.java
+++ surafel/src/surafel/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is hello world");
+		System.out.println("This is Surafel");
 
 	}
 
