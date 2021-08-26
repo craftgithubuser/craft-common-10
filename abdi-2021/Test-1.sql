0980ce368807a30fd2503c6debb42883b19cb1c0
motti12 <abdimotti@gmail.com>
2021-08-26 18:58:34 -0400

    test file

---
+++ abdi-2021/Test-1.sql
@@ -0,0 +1,5 @@
+create schema if not exists studentinfo;
+use studentinfo;
+create schema studentinfo; use studentinfo; 
+create table student( studentid int, studentname varchar(20),
+ parentname varchar(20), adress varchar (40), postalcode int, city varchar (10) );
