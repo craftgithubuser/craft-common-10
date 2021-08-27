e4ba423f1213616d7de722d3683ff0c5402fa6c0
tezerabe <88900356+tezerabe@users.noreply.github.com>
2021-08-27 09:20:22 -0700

    this is quize 3

--- Tezera Assignment/Qize3.DDL.sql
+++ Tezera Assignment/Qize3.DDL.sql
@@ -22,3 +22,18 @@ alter table customers2
 drop column bonus; 
 Rename table 
 customers2 to customertable;
+drop table customertable;
+drop schema mydb;
+CREATE SCHEMA MyDB;
+use MyDB;
+Create table Customers(
+Cust_ID int NOT NULL AUTO_INCREMENT,
+Cust_FirstName varchar(255) NOT NULL,
+Cust_LastName varchar(255) NOT NULL,
+Cust_NickName varchar(255) DEFAULT NULL,
+Cust_Age int DEFAULT NULL,
+Cust_Gender char(1) NOT NULL,
+Cust_Country varchar(255) NOT NULL,
+Cust_Salary double NOT NULL,
+PRIMARY KEY (Cust_ID)
+);
