83bee51560a41c4aae0bdc4643881533dee01258
craftfetig <craftfetuuuz21@gmail.com>
2021-08-27 23:33:21 -0400

    fetiya sql is added

---
+++ fetii branch/fetiya mydb.sql
@@ -0,0 +1,16 @@
+create database MyDB;
+use MyDB;
+
+create table customers(
+cust_ID int auto_increment,
+cust_FirstName varchar(50) null,
+cust_LastName varchar (50)not null,
+cust_NickName varchar(20),
+cust_age int not null,
+cust_Gender char(1) not null,
+cust_Country varchar (10)not null,
+Cust_Salary double not null,
+primary key (Cust_ID)
+);
+
+describe customer;
