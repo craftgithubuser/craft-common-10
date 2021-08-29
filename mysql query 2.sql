6c839eaf9743e5ba960e014981e1be8ce90c8cc3
Seada01 <89057039+Seada01@users.noreply.github.com>
2021-08-28 22:59:19 -0400

    Add files via upload
    sql quiz 2

---
+++ mysql query 2.sql
@@ -0,0 +1,38 @@
+Create database myDb;
+use myDb;
+ Create table customers
+ 
+ (Cust_ID int auto_increment not null,
+ Cust_FirstName varchar(255) not null,
+ Cust_LastName varchar (255) not null,
+ Cust_NickName varchar (255),
+ Cust_Age int,
+ Cust_Gender char(1) not null,
+ Cust_Country varchar(255) not null,
+ Cust_Salary double not null,
+ 
+ primary key (Cust_ID)
+);
+select * from customers;
+
+use myDb;
+
+alter table customers
+add column bonus int check(bonus>=100) not null
+ 
+
+alter table customers
+modify column Cust_Age int default 18 not null
+
+alter table customers
+drop column cust_NickName
+
+alter table customers
+drop column bonus
+
+alter table customers
+rename to CustomersTable 
+
+
+
+describe customers;
