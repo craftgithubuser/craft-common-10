4320dde46d1739980b7cc48bc2b1f91b0ba76230
alexotad <alexo.tadess@gmail.com>
2021-08-31 17:06:25 -0400

    DB Quiz 3

---
+++ Alex DB/DB quiz 3.sql
@@ -0,0 +1,29 @@
+Use MyDB;
+INSERT INTO Customers (Cust_ID, Cust_FirstName, Cust_LastName, Cust_Age, Cust_Gender, Cust_Country, Cust_Salary)
+VALUES (111, 'Nick', 'Jones', 26, 'M', 'USA', 20000000),    
+(222, 'Antony', 'Martial', 24, 'M', 'France', 10000000),
+(333, 'Nebiha', 'Amir', 24, 'F', 'uzbekistan', 36000000),
+(444, 'Soresa', 'Hailu', 24, 'M', 'unknown', 34000000),
+(555, 'Beka', 'Haile', 25, 'M', 'kazakhstan', 400000),
+(666, 'Nick', 'Jones', 26, 'M', 'USA', 20000000),    
+(777, 'Poul', 'Pogba', 26, 'M', 'France', 20000000),
+(888, 'Betty', 'G', 27, 'F', 'Ethiopia', 70000000);
+
+/*UPDATE Customers
+SET Cust_Country = 'Ethiopia'
+WHERE (Cust_ID = 333 and Cust_ID = 444 and Cust_ID = 555);*/
+
+UPDATE Customers
+SET Cust_Country = 'Ethiopia'
+WHERE Cust_ID = 33;
+
+UPDATE Customers
+SET Cust_Country = 'Ethiopia'
+WHERE Cust_ID = 555;
+
+DELETE From customers
+WHERE Cust_ID = 666;
+
+ALTER TABLE customers
+Modify Cust_Salary double;
+
