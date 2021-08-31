934111be0ddbcc31f558931fd0b0e5dccebf3c7a
user <haile.rahel.t@gmail.com>
2021-08-31 14:55:04 -0400

    Rahel M. commit java-git project file 04

---
+++ Java_git_project/src/Java_git_demo/java_git_demo04.java
@@ -0,0 +1,14 @@
+package Java_git_demo;
+
+public class java_git_demo04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GOD IS GOOD ALL THE TIME!");
+		System.out.println("I love Ethiopia!");
+		System.out.println("I love Ethiopia!");
+		
+
+	}
+
+}
