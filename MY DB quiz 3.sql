ec3397dbb611fe22e3b65a502bf512aa0beb87da
Beza2021 <89056014+Beza2021@users.noreply.github.com>
2021-09-01 12:09:35 -0400

    Add files via upload

---
+++ MY DB quiz 3.sql
@@ -0,0 +1,46 @@
+create database mydb;
+use mydb;
+create table custumer(
+
+cust_id int not null auto_increment,
+cust_firstname varchar (255)not null,
+cust_Lastname varchar (255)not null,
+cust_nickname varchar (255),
+cust_age int,
+cust_gender char(1) not null,
+cust_country varchar(255) not null,
+cust_salary double not null,
+primary key (cust_id)
+);
+alter table custumer
+add bonus int check ( bonus> 100)
+
+alter table custumer
+modify column cust_age int default 18 not null;
+alter table custumer
+drop cust_nickname; 
+alter table custumer
+drop bonus;
+rename table custumers to custumer;
+
+drop table custumer;
+drop schema mydb;
+create database mydb;
+use mydb;
+create table custumer(
+
+cust_id int not null auto_increment,
+cust_firstname varchar (255)not null,
+cust_Lastname varchar (255)not null,
+cust_nickname varchar (255),
+cust_age int,
+cust_gender char(1) not null,
+cust_country varchar(255) not null,
+cust_salary double not null,
+primary key (cust_id)
+);
+
+
+
+
+
