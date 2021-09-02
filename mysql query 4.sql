9fc366e837da3f7d909bae32e5b7c330bcd34ddc
Seada01 <89057039+Seada01@users.noreply.github.com>
2021-09-01 21:01:03 -0400

    Add files via upload
    quiz5 sql,

--- mysql query 4.sql
+++ mysql query 4.sql
@@ -40,16 +40,32 @@ delete from customers
 where Cust_ID =666
 
 
+--quiz 4
+use myDB
+truncate table customers;
+start transaction;
 
+insert into customers (Cust_ID, Cust_FirstName, Cust_LastName, Cust_Age, Cust_Gender, Cust_Country, Cust_Salary)    
+values (111, "Nick", "Jones", 26,"M" ,"USA", 20000),
+(222, "Anthony", "Martial", 24, "M", "France", 10000), 
+(333, "Nebiha","Amir", 24, "F", "Uzbekistan", 36000),
+(444, "Soresa", "Hailu", 24, "M", "Unkown", 340000),
+(555, "Beka", "Haile", 25, "M", "Kazakhstan", 40000),
+(666, "Nick", "Jones", 26, "M", "USA", 20000),
+(777, "Paul", "Pogba",26, "M", "France", 200000),
+(888, "Bety", "G", 27, "F", "Ethipia", 70000);   
 
+savepoint ins;
+update customers
+set Cust_Country = "Ethipia"
+where (Cust_ID=333 or Cust_ID=444 or Cust_ID=555);
+savepoint upd
+delete from customers
+where Cust_ID =666;
+savepoint del;
 
-
-
-
-
-
-
-
+Rollback to upd;
+Rollback to ins;
  
-
+commit;
 
