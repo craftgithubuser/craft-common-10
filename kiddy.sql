b7a294cfa89f3944c32fa56e4d4c9b9a58d2e4bf
Kidist Meles <69493598+kidist-kahsay@users.noreply.github.com>
2021-09-04 15:06:37 -0700

    Add files via upload

---
+++ kiddy.sql
@@ -0,0 +1,16 @@
+create Database users;
+use users;
+
+create table Merchants(
+MerchantsID int,
+Merchantsname varchar(255),
+Parentname varchar(255),
+Addres TEXT,
+Age int,
+AdmissionDate Date,
+Payment Double,
+email varchar(255),
+Statuses boolean
+);
+
+
